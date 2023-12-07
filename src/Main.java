cafab3184df3c26066cd25fbf97af8580e4b230e
Jancee <jancee800@gmail.com>
2023-12-08 02:04:45 +0800

    create separate class for Levenshtein functionality

--- src/Main.java
+++ src/Main.java
@@ -247,7 +247,7 @@ public class Main extends Definitions {
         System.out.println("\nWhat is the correct spelling of " + wrongWord + "?");
 
         // Display suggestions
-        List<String> suggestions = getSuggestions(wrongWord, DICTIONARY, 10); // Adjust the number of suggestions as needed
+        List<String> suggestions = Levenshtein.getSuggestions(wrongWord, DICTIONARY, 10);
         System.out.println("Did you mean:");
 
         for (String suggestion : suggestions) {
@@ -272,65 +272,6 @@ public class Main extends Definitions {
             }
             System.out.print("Enter: ");
         }
-    }
-
-    private static List<String> getSuggestions(String misspelledWord, String dictionaryFile, int maxSuggestions) {
-        List<String> suggestions = new ArrayList<>();
-
-        try (Scanner scanner = new Scanner(Paths.get(dictionaryFile))) {
-            while (scanner.hasNext()) {
-                String word = scanner.next().replaceAll("[^a-zA-Z]", "");
-                if (!word.isEmpty()) {
-                    int distance = levenshteinDistance(misspelledWord, word);
-
-                    // Add the word to suggestions if it is within an acceptable edit distance
-                    if (distance <= 3) {  // Adjust the threshold as needed
-                        suggestions.add(word);
-                    }
-
-                    // Sort the suggestions by edit distance (closer words first)
-                    suggestions.sort(Comparator.comparingInt(w -> levenshteinDistance(misspelledWord, w)));
-
-                    // Limit the list to maxSuggestions number of suggestions
-                    if (suggestions.size() >= maxSuggestions) {
-                        break;
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return suggestions;
-    }
-
-    private static int levenshteinDistance(String word1, String word2) {
-        int m = word1.length();
-        int n = word2.length();
-
-        int[][] dp = new int[m + 1][n + 1];
-
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                if (i == 0) {
-                    dp[i][j] = j;
-                } else if (j == 0) {
-                    dp[i][j] = i;
-                } else {
-                    dp[i][j] = min(
-                            dp[i - 1][j] + 1,
-                            dp[i][j - 1] + 1,
-                            dp[i - 1][j - 1] + (word1.charAt(i - 1) == word2.charAt(j - 1) ? 0 : 1)
-                    );
-                }
-            }
-        }
-
-        return dp[m][n];
-    }
-
-    private static int min(int a, int b, int c) {
-        return Math.min(Math.min(a, b), c);
     }
 }
 
